b0387b04cb6e33ba826f1382f9f9b378f92bdfde
johnhjchang <johnhjchang@localhost>
2023-01-26 15:27:48 -0600

    studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("The first of two integers to be averaged?");
+		int n1 = in.nextInt();
+		int n2 = in.nextInt();
+		
+		System.out.println("Average of " + n1 + " and " + n2 + " is " + ((n1 + n2)/2.0));
+		
+	}
+
+}
---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("What is the given year?");
+		int year = in.nextInt();
+		boolean isLeap = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+		System.out.print( year + " is a leap year: " +  isLeap);
+		
+
+	}
+
+}
